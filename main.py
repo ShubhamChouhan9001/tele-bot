1fbd173dff7966690d6eba8a32f8721a1e798dcc
shubham_chouhan <99948905+ShubhamChouhan9001@users.noreply.github.com>
2023-03-04 18:22:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 def main():
     # create a new bot instance using the token provided by BotFather
-    updater = Updater(token='5671089294:AAFmPY1eRenEjVaRa49YCAQQeKbSnUeJViI', use_context=True)
+    updater = Updater(token=5671089294:AAFmPY1eRenEjVaRa49YCAQQeKbSnUeJViI, use_context=True)
 
     # get the dispatcher to register handlers
     dispatcher = updater.dispatcher
